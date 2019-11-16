232345415a41ef9a809ce89ca67da26e7f6a3e0e
Pablo Francisco Mendoza Vargas <pfmv@Pablos-MacBook-Pro.local>
2019-11-16 15:56:09 -0500

    Updates

---
+++ precache-manifest.f211526164e3cc4610a9abb228279934.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b0f7c9b3b76ff7125e874b119f641d",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "696d2e44670d94bd10de",
+    "url": "/Robofriends/static/css/main.a8787daf.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/Robofriends/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "696d2e44670d94bd10de",
+    "url": "/Robofriends/static/js/main.0580c7b8.chunk.js"
+  },
+  {
+    "revision": "cfa90f5f69b5c4be2b91",
+    "url": "/Robofriends/static/js/runtime-main.5f5c191b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
